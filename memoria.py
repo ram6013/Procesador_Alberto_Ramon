853677f809651223a674e7f282d673df1970045d
ram6013 <134057913+ram6013@users.noreply.github.com>
2024-05-18 19:54:06 +0200

    excepciones no archivo + pregunta

--- memoria.py
+++ memoria.py
@@ -2,29 +2,31 @@ from utilidades import *
 
 class MemoriaPrincipal:
     def __init__(self, datos):
-        self.memoria = {}
-        for direccion in range(2**16):
-            self.memoria[dec_a_bin_16b(direccion)] = "0000000000000000"
-        self.carg_mem_fich(datos)
+        self.memoria = {dec_a_bin_16b(direccion): "0000000000000000" for direccion in range(2**16)}
+        if not self.carg_mem_fich(datos):
+            raise ValueError(f"No se pudo cargar la memoria desde el archivo: {datos}")
 
     def g_mem_fich(self, fichero):
-        f = open(fichero, "w")
-        for direccion in self.memoria:
-            f.write(direccion + "\t" + self.memoria[direccion] + "\n")
-        f.close()
+        with open(fichero, "w") as f:
+            for direccion in self.memoria:
+                f.write(direccion + "\t" + self.memoria[direccion] + "\n")
 
     def carg_mem_fich(self, fichero):
         try:
-            f = open(fichero, "r")
-            contenido = f.read().splitlines()
-            f.close()
+            with open(fichero, "r") as f:
+                contenido = f.read().splitlines()
             for linea in contenido:
                 valores_linea = linea.split("\t")
                 direccion = valores_linea[0]
                 valor = valores_linea[1]
                 self.memoria[direccion] = valor
+            return True
         except FileNotFoundError:
-            print("No se encuentra {fichero}")
+            print(f"No se encuentra el archivo {fichero}.")
+            return False
+        except Exception as e:
+            print(f"Error al cargar el archivo {fichero}: {e}.")
+            return False
 
     def cont_dir_mem(self, direccion):
         return self.memoria[direccion]
--- prueba.py
+++ prueba.py
@@ -1,13 +1,14 @@
-from utilidades import *
 from procesador import Procesador
 from memoria import MemoriaPrincipal
 from computador import Computador
 
-alberto_cpu = Procesador(0, 0, 4)
+try:
+    alberto_cpu = Procesador(0, 0, 4)
+    file = str(input("Proporcione el nombre de la memoria que quieres usar.\nEjemplo: memoria_pruebas.txt\n"))
+    # memoria_pruebas o False:
+    alberto_ram = MemoriaPrincipal(file)
 
-# memoria_pruebas o False:
-alberto_ram = MemoriaPrincipal("memoria_pruebas.txt")
-
-alberto_computador = Computador(alberto_cpu, alberto_ram)
-alberto_computador.ini_comp()
-
+    alberto_computador = Computador(alberto_cpu, alberto_ram)
+    alberto_computador.ini_comp()
+except ValueError as e:
+    print(e)  
